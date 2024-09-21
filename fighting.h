bf7760d365fd034d868869d7aea9c648132a35dc
cddoa <166455589+cddoa@users.noreply.github.com>
2024-09-20 17:18:53 -0700

    fighting.h

---
+++ fighting.h
@@ -0,0 +1,9 @@
+#pragma on
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+bool fighting(int playerStriking, int playerGrappling, int playerConditioning, int opponentStriking, int opponentGrappling, int opponentConditioning, std::string opponentName,
+	std::string strikeNames[], int strikeLevels[],int strikeCosts[], std::string topAttackNames[], int topAttackLevels[], int topAttackCosts[], std::string bottomAttackNames[], int bottomAttackLevels[], int bottomAttackCosts[],
+	bool isChampion, bool titleShot, int playerStamina);
+
